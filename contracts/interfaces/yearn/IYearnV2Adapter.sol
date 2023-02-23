90d8ad9991ce7ab4a71c88f16349c82788f80a2c
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-02-23 12:37:34 +0200

    feat: upgrade yearn adapters + tests

--- contracts/interfaces/yearn/IYearnV2Adapter.sol
+++ contracts/interfaces/yearn/IYearnV2Adapter.sol
@@ -1,9 +1,25 @@
 // SPDX-License-Identifier: MIT
 // Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2022
-pragma solidity ^0.8.10;
+// (c) Gearbox Holdings, 2023
+pragma solidity ^0.8.17;
 
 import { IAdapter } from "@gearbox-protocol/core-v2/contracts/interfaces/adapters/IAdapter.sol";
-import { IYVault } from "../../integrations/yearn/IYVault.sol";
 
-interface IYearnV2Adapter is IAdapter, IYVault {}
+interface IYearnV2Adapter is IAdapter {
+    /// @dev Address of the token that is deposited into the vault
+    function token() external view returns (address);
+
+    function deposit() external;
+
+    function deposit(uint256 amount) external;
+
+    function deposit(uint256 amount, address) external;
+
+    function withdraw() external;
+
+    function withdraw(uint256 maxShares) external;
+
+    function withdraw(uint256 maxShares, address) external;
+
+    function withdraw(uint256 maxShares, address, uint256 maxLoss) external;
+}
