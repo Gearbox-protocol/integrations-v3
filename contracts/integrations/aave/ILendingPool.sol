a1ed53ea5f154f6c5254edb324176750c91fc6dc
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-03-10 17:27:51 +0200

    fix: fix waToken exchange rate calculation logic

--- contracts/integrations/aave/ILendingPool.sol
+++ contracts/integrations/aave/ILendingPool.sol
@@ -14,7 +14,6 @@ interface ILendingPool {
      *   is a different wallet
      * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
      *   0 if the action is executed directly by the user, without any middle-man
-     *
      */
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
 
@@ -28,7 +27,6 @@ interface ILendingPool {
      *   wants to receive it on his own wallet, or a different address if the beneficiary is a
      *   different wallet
      * @return The final amount withdrawn
-     *
      */
     function withdraw(address asset, uint256 amount, address to) external returns (uint256);
 
@@ -36,7 +34,13 @@ interface ILendingPool {
      * @dev Returns the state and configuration of the reserve
      * @param asset The address of the underlying asset of the reserve
      * @return The state of the reserve
-     *
      */
     function getReserveData(address asset) external view returns (DataTypes.ReserveData memory);
+
+    /**
+     * @dev Returns the normalized income per unit of asset
+     * @param asset The address of the underlying asset of the reserve
+     * @return The reserve's normalized income
+     */
+    function getReserveNormalizedIncome(address asset) external view returns (uint256);
 }
