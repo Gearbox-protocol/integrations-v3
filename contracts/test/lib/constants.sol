fd2a664a75c4d8eb44388de34ff7f425fa5dd422
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-06-04 23:11:46 +0300

    test: remove tests
    Tests won't compile under the newer version of core-v3.
    We're temporarilly removing them until ready to resolve compatibility issues.
    This commit should be reverted later.

--- contracts/test/lib/constants.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-// Gearbox Protocol. Generalized leverage for DeFi protocols
-// (c) Gearbox Holdings, 2023
-pragma solidity ^0.8.17;
-
-import "@gearbox-protocol/core-v3/contracts/test/lib/constants.sol";
