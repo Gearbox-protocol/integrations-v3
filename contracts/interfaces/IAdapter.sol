7a20c8f0cb16f3b12c6c22582e2009d7346afda4
Dmitry Lekhovitsky <lekhovitsky@gmail.com>
2023-06-05 13:36:38 +0300

    fix: fix imports

--- contracts/interfaces/IAdapter.sol
+++ contracts/interfaces/IAdapter.sol
@@ -3,4 +3,5 @@
 // (c) Gearbox Holdings, 2023
 pragma solidity ^0.8.17;
 
-import {IAdapter, AdapterType} from "@gearbox-protocol/core-v3/contracts/interfaces/IAdapter.sol";
+import {IAdapter} from "@gearbox-protocol/core-v3/contracts/interfaces/IAdapter.sol";
+import {AdapterType} from "@gearbox-protocol/integration-types/contracts/AdapterType.sol";
