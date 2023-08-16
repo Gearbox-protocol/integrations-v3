9f7d55852e26ce6818a6b8ab3b2f7529d8600316
apeir99n <ilgiz.gimaltdinov@gmail.com>
2023-08-16 21:21:30 +0400

    usdc config

--- config/usdcMTconfig.ts
+++ config/usdcMTconfig.ts
@@ -13,49 +13,121 @@ export const config: PoolV3DeployConfig = {
   withdrawalFee: 0,
   expectedLiquidityLimit: BigInt(0),
   irm: {
-    U1: 8000,
+    U1: 7000,
     U2: 9000,
     Rbase: 0,
-    Rslope1: 100,
-    Rslope2: 1000,
+    Rslope1: 150,
+    Rslope2: 400,
     Rslope3: 10000,
     isBorrowingMoreU2Forbidden: true,
   },
   ratesAndLimits: {
+    MKR: {
+      minRate: 0,
+      maxRate: 3000,
+      quotaIncreaseFee: 0,
+      limit: BigInt(1e6) * BigInt(1e6), 
+    }, 
+    UNI: {
+      minRate: 0,
+      maxRate: 3000,
+      quotaIncreaseFee: 0,
+      limit: BigInt(1e6) * BigInt(1e6), 
+    }, 
+    LINK: {
+      minRate: 0,
+      maxRate: 3000,
+      quotaIncreaseFee: 0,
+      limit: BigInt(1e6) * BigInt(1e6), 
+    },
+    LDO: {
+      minRate: 0,
+      maxRate: 3000,
+      quotaIncreaseFee: 0,
+      limit: BigInt(5e5) * BigInt(1e6), 
+    },
+    RPL: {
+      minRate: 0,
+      maxRate: 3000,
+      quotaIncreaseFee: 0,
+      limit: BigInt(3e5) * BigInt(1e6), 
+    },
+    CRV: {
+      minRate: 0,
+      maxRate: 3000,
+      quotaIncreaseFee: 0,
+      limit: BigInt(25e4) * BigInt(1e6), 
+    },
+    APE: {
+      minRate: 0,
+      maxRate: 3000,
+      quotaIncreaseFee: 0,
+      limit: BigInt(3e5) * BigInt(1e6), 
+    },
     CVX: {
       minRate: 10,
-      maxRate: 300,
+      maxRate: 3000,
       quotaIncreaseFee: 0,
-      limit: BigInt(5000000000000),
+      limit: BigInt(3e5) * BigInt(1e6),
     },
     FXS: {
-      minRate: 10,
-      maxRate: 300,
+      minRate: 0,
+      maxRate: 3000,
       quotaIncreaseFee: 0,
-      limit: BigInt(5000000000000),
-    },
-    LQTY: {
-      minRate: 10,
-      maxRate: 300,
-      quotaIncreaseFee: 0,
-      limit: BigInt(5000000000000),
+      limit: BigInt(3e5) * BigInt(1e6),
     },
   },
   creditManagers: [
     {
       degenNft: false,
       expirationDate: undefined,
-      minDebt: BigInt(1e5) * BigInt(1e6),
+      minDebt: BigInt(1e4) * BigInt(1e6),
       maxDebt: BigInt(1e6) * BigInt(1e6),
       poolLimit: BigInt(0),
       collateralTokens: [
         {
+          token: "WETH",
+          lt: 8700,
+        },
+        {
+          token: "WBTC",
+          lt: 8700,
+        },
+        {
+          token: "MKR",
+          lt: 8500,
+        },
+        {
+          token: "UNI",
+          lt: 8500,
+        },
+        {
+          token: "LINK",
+          lt: 8500,
+        },
+        {
+          token: "LDO",
+          lt: 8500,
+        },
+        {
+          token: "RPL",
+          lt: 8500,
+        },
+        {
+          token: "CRV",
+          lt: 8500,
+        },
+        {
+          token: "APE",
+          lt: 8500,
+        },
+        {
           token: "CVX",
-          lt: 2500,
+          lt: 8500,
         },
         {
           token: "FXS",
-          lt: 2000,
+          lt: 8500,
         },
       ],
       adapters: ["UNISWAP_V3_ROUTER"],
