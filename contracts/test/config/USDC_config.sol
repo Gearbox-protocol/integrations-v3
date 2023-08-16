8f4115be4e9c9707405120a71b74d1a2c988b44e
0xmikko <26343374+0xmikko@users.noreply.github.com>
2023-08-16 14:04:17 +0200

    fix: test env

--- contracts/test/config/USDC_config.sol
+++ contracts/test/config/USDC_config.sol
@@ -18,24 +18,25 @@ import {
 
 contract CONFIG_MAINNET_DUSDC_V3 is IPoolV3DeployConfig {
     string public constant id = "mainnet-dusdc-v3";
-    string public constant symbol = "dUSDCV3";
-    string public constant name = "Diesel USDC V3 pool";
     uint256 public constant chainId = 1;
-
     Tokens public constant underlying = Tokens.USDC;
     bool public constant supportsQuotas = false;
+    uint256 public constant getAccountAmount = 1_000_000_000_000;
 
-    uint256 public constant getAccountAmount = 1000000000000;
+    // POOL
+
+    string public constant symbol = "dUSDCV3";
+    string public constant name = "Diesel USDC V3 pool";
 
     PoolV3DeployParams _poolParams = PoolV3DeployParams({withdrawalFee: 0, expectedLiquidityLimit: 0});
 
     LinearIRMV3DeployParams _irm = LinearIRMV3DeployParams({
-        U_1: 8000,
-        U_2: 9000,
+        U_1: 80_00,
+        U_2: 90_00,
         R_base: 0,
-        R_slope1: 100,
-        R_slope2: 1000,
-        R_slope3: 10000,
+        R_slope1: 1_00,
+        R_slope2: 10_00,
+        R_slope3: 100_00,
         _isBorrowingMoreU2Forbidden: true
     });
 
@@ -45,27 +46,27 @@ contract CONFIG_MAINNET_DUSDC_V3 is IPoolV3DeployConfig {
     CreditManagerV3DeployParams[] _creditManagers;
 
     constructor() {
-        _gaugeRates.push(GaugeRate({token: Tokens.CVX, minRate: 10, maxRate: 300}));
-        _gaugeRates.push(GaugeRate({token: Tokens.FXS, minRate: 10, maxRate: 300}));
-        _gaugeRates.push(GaugeRate({token: Tokens.LQTY, minRate: 10, maxRate: 300}));
-        _quotaLimits.push(PoolQuotaLimit({token: Tokens.CVX, quotaIncreaseFee: 0, limit: 5000000000000}));
-        _quotaLimits.push(PoolQuotaLimit({token: Tokens.FXS, quotaIncreaseFee: 0, limit: 5000000000000}));
-        _quotaLimits.push(PoolQuotaLimit({token: Tokens.LQTY, quotaIncreaseFee: 0, limit: 5000000000000}));
+        _gaugeRates.push(GaugeRate({token: Tokens.CVX, minRate: 10, maxRate: 3_00}));
+        _gaugeRates.push(GaugeRate({token: Tokens.FXS, minRate: 10, maxRate: 3_00}));
+        _gaugeRates.push(GaugeRate({token: Tokens.LQTY, minRate: 10, maxRate: 3_00}));
+        _quotaLimits.push(PoolQuotaLimit({token: Tokens.CVX, quotaIncreaseFee: 0, limit: 5_000_000_000_000}));
+        _quotaLimits.push(PoolQuotaLimit({token: Tokens.FXS, quotaIncreaseFee: 0, limit: 5_000_000_000_000}));
+        _quotaLimits.push(PoolQuotaLimit({token: Tokens.LQTY, quotaIncreaseFee: 0, limit: 5_000_000_000_000}));
 
         /// CREDIT_MANAGER_0
         CreditManagerV3DeployParams storage cp = _creditManagers.push();
 
-        cp.minDebt = 100000000000;
-        cp.maxDebt = 1000000000000;
+        cp.minDebt = 100_000_000_000;
+        cp.maxDebt = 1_000_000_000_000;
         cp.whitelisted = false;
         cp.expirable = false;
         cp.skipInit = false;
         cp.poolLimit = 0;
 
         CollateralTokenHuman[] storage cts = cp.collateralTokens;
-        cts.push(CollateralTokenHuman({token: Tokens.CVX, lt: 2500}));
+        cts.push(CollateralTokenHuman({token: Tokens.CVX, lt: 25_00}));
 
-        cts.push(CollateralTokenHuman({token: Tokens.FXS, lt: 2000}));
+        cts.push(CollateralTokenHuman({token: Tokens.FXS, lt: 20_00}));
         Contracts[] storage cs = cp.contracts;
         cs.push(Contracts.UNISWAP_V3_ROUTER);
     }
--- contracts/test/live/Env.sol
+++ contracts/test/live/Env.sol
@@ -5,7 +5,12 @@ pragma solidity ^0.8.17;
 
 import {LiveTestHelper} from "../suites/LiveTestHelper.sol";
 // import {BalanceComparator, BalanceBackup} from "../../../helpers/BalanceComparator.sol";
+import {CONFIG_MAINNET_DUSDC_V3_ROUTER} from "../config/USDC_router_config.sol";
 
 contract Live_LidoEquivalenceTest is LiveTestHelper {
-    function test_tt() public liveCreditTest("mainnet-dusdc-v3") {}
+    function setUp() public {
+        addDeployConfig(new CONFIG_MAINNET_DUSDC_V3_ROUTER());
+    }
+
+    function test_tt() public liveCreditTest("mainnet-dusdc-v3-router") {}
 }
