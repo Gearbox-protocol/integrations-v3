a7dc44c6ee278ca3da615af2d0210bab68843d19
Van0k <ivkamakin@gmail.com>
2023-08-23 11:08:50 +0400

    feat: test deploy adapter config + convex fix

--- contracts/test/suites/LiveTestHelper.sol
+++ contracts/test/suites/LiveTestHelper.sol
@@ -3,9 +3,17 @@
 // (c) Gearbox Foundation, 2023.
 pragma solidity ^0.8.10;
 
+import {ICreditManagerV3} from "@gearbox-protocol/core-v3/contracts/interfaces/ICreditManagerV3.sol";
+
 import {Tokens} from "@gearbox-protocol/sdk-gov/contracts/Tokens.sol";
 import {SupportedContracts, Contracts} from "@gearbox-protocol/sdk-gov/contracts/SupportedContracts.sol";
-import {IPoolV3DeployConfig} from "@gearbox-protocol/core-v3/contracts/test/interfaces/ICreditConfig.sol";
+import {
+    IPoolV3DeployConfig,
+    CreditManagerV3DeployParams,
+    BalancerPool,
+    UniswapV3Pair,
+    UniswapV2Pair
+} from "@gearbox-protocol/core-v3/contracts/test/interfaces/ICreditConfig.sol";
 
 import {PriceFeedDeployer} from "@gearbox-protocol/oracles-v3/contracts/test/suites/PriceFeedDeployer.sol";
 import {IntegrationTestHelper} from "@gearbox-protocol/core-v3/contracts/test/helpers/IntegrationTestHelper.sol";
@@ -16,6 +24,16 @@ import {CONFIG_MAINNET_WBTC_MT_V3} from "../config/WBTC_MT_config.sol";
 import {CONFIG_MAINNET_WETH_MT_V3} from "../config/WETH_MT_config.sol";
 
 import {CONFIG_MAINNET_USDC_LEV_V3} from "../config/USDC_Lev_config.sol";
+
+import {IConvexV1BoosterAdapter} from "../../interfaces/convex/IConvexV1BoosterAdapter.sol";
+import {BalancerV2VaultAdapter} from "../../adapters/balancer/BalancerV2VaultAdapter.sol";
+import {UniswapV2Adapter} from "../../adapters/uniswap/UniswapV2.sol";
+import {UniswapV3Adapter} from "../../adapters/uniswap/UniswapV3.sol";
+import {PoolStatus} from "../../interfaces/balancer/IBalancerV2VaultAdapter.sol";
+import {UniswapV2PairStatus} from "../../interfaces/uniswap/IUniswapV2Adapter.sol";
+import {UniswapV3PoolStatus} from "../../interfaces/uniswap/IUniswapV3Adapter.sol";
+
+import "@gearbox-protocol/core-v3/contracts/test/lib/constants.sol";
 
 import "forge-std/console.sol";
 
@@ -62,12 +80,86 @@ contract LiveTestHelper is IntegrationTestHelper {
             new AdapterDeployer(address(creditManagers[i]), config.creditManagers()[i].contracts, tokenTestSuite, supportedContracts );
 
             adapterDeployer.connectAdapters();
-            _configureAdapters();
+            _configureAdapters(address(creditManagers[i]), config.creditManagers()[i]);
         }
     }
 
-    function _configureAdapters() internal {
-        /// Config your adapoters here!
+    function _configureAdapters(address creditManager, CreditManagerV3DeployParams memory creditManagerParams)
+        internal
+    {
+        // CONVEX BOOSTER
+        address boosterAdapter = getAdapter(creditManager, Contracts.CONVEX_BOOSTER);
+
+        vm.prank(CONFIGURATOR);
+        IConvexV1BoosterAdapter(boosterAdapter).updateStakedPhantomTokensMap();
+
+        // BALANCER VAULT
+        BalancerPool[] memory bPools = creditManagerParams.balancerPools;
+
+        if (bPools.length != 0) {
+            address balancerAdapter = getAdapter(creditManager, Contracts.BALANCER_VAULT);
+
+            for (uint256 i = 0; i < bPools.length; ++i) {
+                vm.prank(CONFIGURATOR);
+                BalancerV2VaultAdapter(balancerAdapter).setPoolStatus(bPools[i].poolId, PoolStatus(bPools[i].status));
+            }
+        }
+        // UNISWAP V3 ROUTER
+        UniswapV3Pair[] memory uniV3Pools = creditManagerParams.uniswapV3Pairs;
+
+        if (uniV3Pools.length != 0) {
+            UniswapV3PoolStatus[] memory pools = new UniswapV3PoolStatus[](uniV3Pools.length);
+
+            for (uint256 i = 0; i < uniV3Pools.length; ++i) {
+                pools[i] = UniswapV3PoolStatus({
+                    token0: tokenTestSuite.addressOf(uniV3Pools[i].token0),
+                    token1: tokenTestSuite.addressOf(uniV3Pools[i].token1),
+                    fee: uniV3Pools[i].fee,
+                    allowed: true
+                });
+            }
+
+            address uniV3Adapter = getAdapter(creditManager, Contracts.UNISWAP_V3_ROUTER);
+
+            vm.prank(CONFIGURATOR);
+            UniswapV3Adapter(uniV3Adapter).setPoolStatusBatch(pools);
+        }
+        // UNISWAP V2 AND SUSHISWAP
+        UniswapV2Pair[] memory uniV2Pairs = creditManagerParams.uniswapV2Pairs;
+
+        if (uniV2Pairs.length != 0) {
+            UniswapV2PairStatus[] memory pairs = new UniswapV2PairStatus[](uniV2Pairs.length);
+
+            for (uint256 i = 0; i < uniV2Pairs.length; ++i) {
+                if (uniV2Pairs[i].router != Contracts.UNISWAP_V2_ROUTER) continue;
+                pairs[i] = UniswapV2PairStatus({
+                    token0: tokenTestSuite.addressOf(uniV2Pairs[i].token0),
+                    token1: tokenTestSuite.addressOf(uniV2Pairs[i].token1),
+                    allowed: true
+                });
+            }
+
+            address uniV2Adapter = getAdapter(creditManager, Contracts.UNISWAP_V2_ROUTER);
+
+            vm.prank(CONFIGURATOR);
+            UniswapV2Adapter(uniV2Adapter).setPairStatusBatch(pairs);
+
+            pairs = new UniswapV2PairStatus[](uniV2Pairs.length);
+
+            for (uint256 i = 0; i < uniV2Pairs.length; ++i) {
+                if (uniV2Pairs[i].router != Contracts.SUSHISWAP_ROUTER) continue;
+                pairs[i] = UniswapV2PairStatus({
+                    token0: tokenTestSuite.addressOf(uniV2Pairs[i].token0),
+                    token1: tokenTestSuite.addressOf(uniV2Pairs[i].token1),
+                    allowed: true
+                });
+            }
+
+            address sushiAdapter = getAdapter(creditManager, Contracts.SUSHISWAP_ROUTER);
+
+            vm.prank(CONFIGURATOR);
+            UniswapV2Adapter(sushiAdapter).setPairStatusBatch(pairs);
+        }
     }
 
     function _setUp() public virtual liveTest {
@@ -120,9 +212,9 @@ contract LiveTestHelper is IntegrationTestHelper {
     //         }
     //     }
 
-    // function getAdapter(address creditManager, Contracts target) public view returns (address) {
-    //     return creditManager(creditManager).contractToAdapter(supportedContracts.addressOf(target));
-    // }
+    function getAdapter(address creditManager, Contracts target) public view returns (address) {
+        return ICreditManagerV3(creditManager).contractToAdapter(supportedContracts.addressOf(target));
+    }
 
     // function getAdapter(Tokens underlying, Contracts target) public view returns (address) {
     //     return _creditManagers[underlying][0].contractToAdapter(supportedContracts.addressOf(target));
