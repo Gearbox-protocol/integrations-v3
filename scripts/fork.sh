625acf49ad361b80d22e98c28a10b01aff845f39
Mikael <26343374+0xmikko@users.noreply.github.com>
2023-09-14 10:58:07 +0200

    Merge branch 'main' of github.com:Gearbox-protocol/integrations-v3

--- scripts/fork.sh
+++
@@ -1,9 +0,0 @@
-set -o allexport; source ./.env; set +o allexport;
-export $(grep -v '^#' .env | xargs -d '\n')
-
-if [[ -z "${ETH_MAINNET_BLOCK}" ]]; then
-  anvil -f $ETH_MAINNET_PROVIDER  --chain-id 1337
-else
-  anvil -f $ETH_MAINNET_PROVIDER --fork-block-number $ETH_MAINNET_BLOCK --chain-id 1337
-fi
-
