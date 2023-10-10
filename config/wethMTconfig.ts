297bc9a8fa216ba5c892faee23ab7b833993a29f
Maksim Manylov <manylov@gmail.com>
2023-10-11 03:02:55 +0600

    fix: minRate

--- config/wethMTconfig.ts
+++ config/wethMTconfig.ts
@@ -23,13 +23,13 @@ export const config: PoolV3DeployConfig = {
   },
   ratesAndLimits: {
     STETH: {
-      minRate: 0,
+      minRate: 1,
       maxRate: 200,
       quotaIncreaseFee: 0,
       limit: (BigInt(30e6) * POOL_DECIMALS) / POOL_DIVIDER,
     },
     USDC: {
-      minRate: 0,
+      minRate: 1,
       maxRate: 3000,
       quotaIncreaseFee: 100,
       limit: (BigInt(30e6) * POOL_DECIMALS) / POOL_DIVIDER,
