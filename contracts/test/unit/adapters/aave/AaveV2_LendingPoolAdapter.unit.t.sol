aea2c1a05415639867ee1766344ac9a4378bfcc1
Dima Lekhovitsky <lekhovitsky@gmail.com>
2023-10-26 15:13:48 +0300

    test: cleanup

--- contracts/test/unit/adapters/aave/AaveV2_LendingPoolAdapter.unit.t.sol
+++ contracts/test/unit/adapters/aave/AaveV2_LendingPoolAdapter.unit.t.sol
@@ -3,256 +3,143 @@
 // (c) Gearbox Foundation, 2023.
 pragma solidity ^0.8.17;
 
-import {USER, CONFIGURATOR} from "@gearbox-protocol/core-v3/contracts/test/lib/constants.sol";
 import {AaveV2_LendingPoolAdapter} from "../../../../adapters/aave/AaveV2_LendingPoolAdapter.sol";
-import {Tokens} from "@gearbox-protocol/sdk-gov/contracts/Tokens.sol";
-import {AaveTestHelper} from "./AaveTestHelper.sol";
-import "@gearbox-protocol/core-v3/contracts/interfaces/IExceptions.sol";
+import {ILendingPool, DataTypes} from "../../../../integrations/aave/ILendingPool.sol";
+import {AdapterUnitTestHelper} from "../AdapterUnitTestHelper.sol";
 
-/// @title Aave V2 lending pool adapter tests
-/// @notice [AAV2LP]: Unit tests for Aave V2 lending pool adapter
-contract AaveV2_LendingPoolAdapter_Test is AaveTestHelper {
-    AaveV2_LendingPoolAdapter public adapter;
+/// @title Aave v2 lending pool adapter unit test
+/// @notice U:[AAVE2]: Unit tests for Aave v2 lending pool adapter
+contract AaveV2_LendingPoolAdapterUnitTest is AdapterUnitTestHelper {
+    AaveV2_LendingPoolAdapter adapter;
+
+    address lendingPool;
 
     function setUp() public {
-        _setupAaveSuite(false);
+        _setUp();
 
-        // create a lending pool adapter and add it to the credit manager
-        vm.startPrank(CONFIGURATOR);
-        adapter = new AaveV2_LendingPoolAdapter(address(creditManager), address(lendingPool));
-        creditConfigurator.allowAdapter(address(adapter));
-        vm.label(address(adapter), "LENDING_POOL_ADAPTER");
-        vm.stopPrank();
+        lendingPool = makeAddr("LENDING_POOL");
+
+        DataTypes.ReserveData memory data;
+        data.aTokenAddress = tokens[1];
+        vm.mockCall(lendingPool, abi.encodeCall(ILendingPool.getReserveData, (tokens[0])), abi.encode(data));
+
+        adapter = new AaveV2_LendingPoolAdapter(address(creditManager), lendingPool);
     }
 
-    /// @notice [AAV2LP-1]: All action functions revert if called not from the multicall
-    function test_AAV2LP_01_action_functions_revert_if_called_not_from_multicall() public {
-        vm.prank(USER);
-        vm.expectRevert(CallerNotCreditFacadeException.selector);
-        adapter.deposit(dai, 1, address(0), 0);
-
-        vm.prank(USER);
-        vm.expectRevert(CallerNotCreditFacadeException.selector);
-        adapter.depositAll(dai);
-
-        vm.prank(USER);
-        vm.expectRevert(CallerNotCreditFacadeException.selector);
-        adapter.withdraw(dai, 1, address(0));
-
-        vm.prank(USER);
-        vm.expectRevert(CallerNotCreditFacadeException.selector);
-        adapter.withdrawAll(dai);
+    /// @notice U:[AAVE2-1]: Constructor works as expected
+    function test_U_AAVE2_01_constructor_works_as_expected() public {
+        assertEq(adapter.creditManager(), address(creditManager), "Incorrect creditManager");
+        assertEq(adapter.addressProvider(), address(addressProvider), "Incorrect addressProvider");
+        assertEq(adapter.targetContract(), lendingPool, "Incorrect targetContract");
     }
 
-    /// @notice [AAV2LP-2]: All action functions revert if called on not registered token
-    function test_AAV2LP_02_action_functions_revert_if_called_on_not_registered_token() public {
-        (address creditAccount, uint256 balance) = _openTestCreditAccount();
-        tokenTestSuite.approve(dai, creditAccount, address(lendingPool), balance / 2);
-        vm.prank(creditAccount);
-        lendingPool.deposit(dai, balance / 2, creditAccount, 0);
+    /// @notice U:[AAVE2-2]: Wrapper functions revert on wrong caller
+    function test_U_AAVE2_02_wrapper_functions_revert_on_wrong_caller() public {
+        _revertsOnNonFacadeCaller();
+        adapter.deposit(address(0), 0, address(0), 0);
 
-        vm.expectRevert(TokenNotAllowedException.selector);
-        executeOneLineMulticall(
-            creditAccount, address(adapter), abi.encodeCall(adapter.deposit, (dai, balance / 2, address(0), 0))
-        );
+        _revertsOnNonFacadeCaller();
+        adapter.depositAll(address(0));
 
-        vm.expectRevert(TokenNotAllowedException.selector);
-        executeOneLineMulticall(creditAccount, address(adapter), abi.encodeCall(adapter.depositAll, (dai)));
+        _revertsOnNonFacadeCaller();
+        adapter.withdraw(address(0), 0, address(0));
 
-        vm.expectRevert(TokenNotAllowedException.selector);
-        executeOneLineMulticall(
-            creditAccount, address(adapter), abi.encodeCall(adapter.withdraw, (dai, balance / 2, address(0)))
-        );
-
-        vm.expectRevert(TokenNotAllowedException.selector);
-        executeOneLineMulticall(creditAccount, address(adapter), abi.encodeCall(adapter.withdrawAll, (dai)));
+        _revertsOnNonFacadeCaller();
+        adapter.withdrawAll(address(0));
     }
 
-    /// @notice [AAV2LP-3]: `deposit` works correctly
-    /// @dev Fuzzing time before deposit to see if adapter handles interest properly
-    /// @dev Resulting aToken balance is allowed to deviate from the expected value by at most 1
-    ///      due to rounding errors in rebalancing
-    function test_AAV2LP_03_deposit_works_correctly(uint256 timedelta) public {
-        vm.assume(timedelta < 3 * 365 days);
-        uint256 snapshot = vm.snapshot();
+    /// @notice U:[AAVE2-3]: `deposit` works as expected
+    function test_U_AAVE2_03_deposit_works_as_expected() public {
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: tokens[0],
+            tokenOut: tokens[1],
+            callData: abi.encodeCall(ILendingPool.deposit, (tokens[0], 123, creditAccount, 0)),
+            requiresApproval: true,
+            validatesTokens: true
+        });
 
-        for (uint256 isUsdc; isUsdc < 2; ++isUsdc) {
-            (address creditAccount, uint256 initialBalance) =
-                _openAccountWithToken(isUsdc == 1 ? Tokens.USDC : Tokens.WETH);
-            (address token, address aToken) = isUsdc == 1 ? (usdc, aUsdc) : (weth, aWeth);
+        vm.prank(creditFacade);
+        (uint256 tokensToEnable, uint256 tokensToDisable) = adapter.deposit(tokens[0], 123, address(0), 0);
 
-            expectAllowance(token, creditAccount, address(lendingPool), 0);
-            vm.warp(block.timestamp + timedelta);
-
-            uint256 depositAmount = initialBalance / 2;
-
-            bytes memory expectedCallData =
-                abi.encodeCall(lendingPool.deposit, (token, depositAmount, creditAccount, 0));
-            expectMulticallStackCalls(
-                address(adapter), address(lendingPool), USER, expectedCallData, token, aToken, true
-            );
-
-            bytes memory callData = abi.encodeCall(adapter.deposit, (token, depositAmount, address(0), 0));
-            executeOneLineMulticall(creditAccount, address(adapter), callData);
-
-            expectBalance(token, creditAccount, initialBalance - depositAmount);
-            // expectBalance(aToken, creditAccount, depositAmount);
-            expectBalanceGe(aToken, creditAccount, depositAmount - 1, "");
-            expectBalanceLe(aToken, creditAccount, depositAmount + 1, "");
-
-            expectAllowance(token, creditAccount, address(lendingPool), 1);
-
-            expectTokenIsEnabled(creditAccount, token, true);
-            expectTokenIsEnabled(creditAccount, aToken, true);
-
-            vm.revertTo(snapshot);
-        }
+        assertEq(tokensToEnable, 2, "Incorrect tokensToEnable");
+        assertEq(tokensToDisable, 0, "Incorrect tokensToDisable");
     }
 
-    /// @notice [AAV2LP-4]: `depositAll` works correctly
-    /// @dev Fuzzing time before deposit to see if adapter handles interest properly
-    /// @dev Resulting aToken balance is allowed to deviate from the expected value by at most 1
-    ///      due to rounding errors in rebalancing
-    function test_AAV2LP_04_depositAll_works_correctly(uint256 timedelta) public {
-        vm.assume(timedelta < 3 * 365 days);
-        uint256 snapshot = vm.snapshot();
+    /// @notice U:[AAVE2-4]: `depositAll` works as expected
+    function test_U_AAVE2_04_depositAll_works_as_expected() public {
+        deal({token: tokens[0], to: creditAccount, give: 1001});
 
-        for (uint256 isUsdc; isUsdc < 2; ++isUsdc) {
-            (address creditAccount, uint256 initialBalance) =
-                _openAccountWithToken(isUsdc == 1 ? Tokens.USDC : Tokens.WETH);
-            (address token, address aToken) = isUsdc == 1 ? (usdc, aUsdc) : (weth, aWeth);
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: tokens[0],
+            tokenOut: tokens[1],
+            callData: abi.encodeCall(ILendingPool.deposit, (tokens[0], 1000, creditAccount, 0)),
+            requiresApproval: true,
+            validatesTokens: true
+        });
 
-            expectAllowance(token, creditAccount, address(lendingPool), 0);
-            vm.warp(block.timestamp + timedelta);
+        vm.prank(creditFacade);
+        (uint256 tokensToEnable, uint256 tokensToDisable) = adapter.depositAll(tokens[0]);
 
-            bytes memory expectedCallData =
-                abi.encodeCall(lendingPool.deposit, (token, initialBalance - 1, creditAccount, 0));
-            expectMulticallStackCalls(
-                address(adapter), address(lendingPool), USER, expectedCallData, token, aToken, true
-            );
-
-            bytes memory callData = abi.encodeCall(adapter.depositAll, (token));
-            executeOneLineMulticall(creditAccount, address(adapter), callData);
-
-            expectBalance(token, creditAccount, 1);
-            // expectBalance(aToken, creditAccount, initialBalance - 1);
-            expectBalanceGe(aToken, creditAccount, initialBalance - 2, "");
-            expectBalanceLe(aToken, creditAccount, initialBalance, "");
-
-            expectAllowance(token, creditAccount, address(lendingPool), 1);
-
-            expectTokenIsEnabled(creditAccount, token, false);
-            expectTokenIsEnabled(creditAccount, aToken, true);
-
-            vm.revertTo(snapshot);
-        }
+        assertEq(tokensToEnable, 2, "Incorrect tokensToEnable");
+        assertEq(tokensToDisable, 1, "Incorrect tokensToDisable");
     }
 
-    /// @notice [AAV2LP-5A]: `withdraw` works correctly
-    /// @dev Fuzzing time before withdrawal to see if adapter handles interest properly
-    /// @dev Resulting aToken balance is allowed to deviate from the expected value by at most 1
-    ///      due to rounding errors in rebalancing
-    function test_AAV2LP_05A_withdraw_works_correctly(uint256 timedelta) public {
-        vm.assume(timedelta < 3 * 365 days);
-        uint256 snapshot = vm.snapshot();
+    /// @notice U:[AAVE2-5A]: `withdraw` works as expected
+    function test_U_AAVE2_05A_withdraw_works_as_expected() public {
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: tokens[1],
+            tokenOut: tokens[0],
+            callData: abi.encodeCall(ILendingPool.withdraw, (tokens[0], 123, creditAccount)),
+            requiresApproval: false,
+            validatesTokens: true
+        });
 
-        for (uint256 isUsdc; isUsdc < 2; ++isUsdc) {
-            (address token, address aToken) = isUsdc == 1 ? (usdc, aUsdc) : (weth, aWeth);
-            (address creditAccount,) = _openAccountWithAToken(isUsdc == 1 ? Tokens.USDC : Tokens.WETH);
+        vm.prank(creditFacade);
+        (uint256 tokensToEnable, uint256 tokensToDisable) = adapter.withdraw(tokens[0], 123, address(0));
 
-            vm.warp(block.timestamp + timedelta);
-            uint256 initialBalance = tokenTestSuite.balanceOf(aToken, creditAccount);
-            uint256 withdrawAmount = initialBalance / 2;
-
-            bytes memory expectedCallData = abi.encodeCall(lendingPool.withdraw, (token, withdrawAmount, creditAccount));
-            expectMulticallStackCalls(
-                address(adapter), address(lendingPool), USER, expectedCallData, aToken, token, false
-            );
-
-            bytes memory callData = abi.encodeCall(adapter.withdraw, (token, withdrawAmount, creditAccount));
-            executeOneLineMulticall(creditAccount, address(adapter), callData);
-
-            expectBalance(token, creditAccount, withdrawAmount);
-            // expectBalance(aToken, creditAccount, initialBalance - withdrawAmount);
-            expectBalanceGe(aToken, creditAccount, initialBalance - withdrawAmount - 1, "");
-            expectBalanceLe(aToken, creditAccount, initialBalance - withdrawAmount + 1, "");
-
-            expectAllowance(aToken, creditAccount, address(lendingPool), 0);
-
-            expectTokenIsEnabled(creditAccount, token, true);
-            expectTokenIsEnabled(creditAccount, aToken, true);
-
-            vm.revertTo(snapshot);
-        }
+        assertEq(tokensToEnable, 1, "Incorrect tokensToEnable");
+        assertEq(tokensToDisable, 0, "Incorrect tokensToDisable");
     }
 
-    /// @notice [AAV2LP-5B]: `withdraw` works correctly when `amount == type(uint256).max`
-    /// @dev Fuzzing time before withdrawal to see if adapter handles interest properly
-    /// @dev Resulting aToken balance is allowed to deviate from the expected value by at most 1
-    ///      due to rounding errors in rebalancing
-    function test_AAV2LP_05B_withdraw_works_correctly(uint256 timedelta) public {
-        vm.assume(timedelta < 3 * 365 days);
-        uint256 snapshot = vm.snapshot();
+    /// @notice U:[AAVE2-5B]: `withdraw` works as expected with amx amount
+    function test_U_AAVE2_05B_withdraw_works_as_expected_with_max_amount() public {
+        deal({token: tokens[1], to: creditAccount, give: 1001});
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: tokens[1],
+            tokenOut: tokens[0],
+            callData: abi.encodeCall(ILendingPool.withdraw, (tokens[0], 1000, creditAccount)),
+            requiresApproval: false,
+            validatesTokens: true
+        });
 
-        for (uint256 isUsdc; isUsdc < 2; ++isUsdc) {
-            (address token, address aToken) = isUsdc == 1 ? (usdc, aUsdc) : (weth, aWeth);
-            (address creditAccount,) = _openAccountWithAToken(isUsdc == 1 ? Tokens.USDC : Tokens.WETH);
+        vm.prank(creditFacade);
+        (uint256 tokensToEnable, uint256 tokensToDisable) = adapter.withdraw(tokens[0], type(uint256).max, address(0));
 
-            vm.warp(block.timestamp + timedelta);
-            uint256 initialBalance = tokenTestSuite.balanceOf(aToken, creditAccount);
-
-            bytes memory expectedCallData =
-                abi.encodeCall(lendingPool.withdraw, (token, initialBalance - 1, creditAccount));
-            expectMulticallStackCalls(
-                address(adapter), address(lendingPool), USER, expectedCallData, aToken, token, false
-            );
-
-            bytes memory callData = abi.encodeCall(adapter.withdraw, (token, type(uint256).max, creditAccount));
-            executeOneLineMulticall(creditAccount, address(adapter), callData);
-
-            expectBalance(token, creditAccount, initialBalance - 1);
-            // expectBalance(aToken, creditAccount, 1);
-            expectBalanceLe(aToken, creditAccount, 2, "");
-
-            expectTokenIsEnabled(creditAccount, token, true);
-            expectTokenIsEnabled(creditAccount, aToken, false);
-
-            vm.revertTo(snapshot);
-        }
+        assertEq(tokensToEnable, 1, "Incorrect tokensToEnable");
+        assertEq(tokensToDisable, 2, "Incorrect tokensToDisable");
     }
 
-    /// @notice [AAV2LP-6]: `withdrawAll` works correctly
-    /// @dev Fuzzing time before withdrawal to see if adapter handles interest properly
-    /// @dev Resulting aToken balance is allowed to deviate from the expected value by at most 1
-    ///      due to rounding errors in rebalancing
-    function test_AAV2LP_06_withdrawAll_works_correctly(uint256 timedelta) public {
-        vm.assume(timedelta < 3 * 365 days);
-        uint256 snapshot = vm.snapshot();
+    /// @notice U:[AAVE2-6]: `withdrawAll` works as expected
+    function test_U_AAVE2_06_withdrawAll_works_as_expected() public {
+        deal({token: tokens[1], to: creditAccount, give: 1001});
 
-        for (uint256 isUsdc; isUsdc < 2; ++isUsdc) {
-            (address token, address aToken) = isUsdc == 1 ? (usdc, aUsdc) : (weth, aWeth);
-            (address creditAccount,) = _openAccountWithAToken(isUsdc == 1 ? Tokens.USDC : Tokens.WETH);
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: tokens[1],
+            tokenOut: tokens[0],
+            callData: abi.encodeCall(ILendingPool.withdraw, (tokens[0], 1000, creditAccount)),
+            requiresApproval: false,
+            validatesTokens: true
+        });
 
-            vm.warp(block.timestamp + timedelta);
-            uint256 initialBalance = tokenTestSuite.balanceOf(aToken, creditAccount);
+        vm.prank(creditFacade);
+        (uint256 tokensToEnable, uint256 tokensToDisable) = adapter.withdrawAll(tokens[0]);
 
-            bytes memory expectedCallData =
-                abi.encodeCall(lendingPool.withdraw, (token, initialBalance - 1, creditAccount));
-            expectMulticallStackCalls(
-                address(adapter), address(lendingPool), USER, expectedCallData, aToken, token, false
-            );
-
-            bytes memory callData = abi.encodeCall(adapter.withdrawAll, (token));
-            executeOneLineMulticall(creditAccount, address(adapter), callData);
-
-            expectBalance(token, creditAccount, initialBalance - 1);
-            // expectBalance(aToken, creditAccount, 1);
-            expectBalanceLe(aToken, creditAccount, 2, "");
-
-            expectTokenIsEnabled(creditAccount, token, true);
-            expectTokenIsEnabled(creditAccount, aToken, false);
-
-            vm.revertTo(snapshot);
-        }
+        assertEq(tokensToEnable, 1, "Incorrect tokensToEnable");
+        assertEq(tokensToDisable, 2, "Incorrect tokensToDisable");
     }
 }
