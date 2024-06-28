6a3654c1a7f7bcade0c837b441d8d3158bea82e2
Van0k <ivkamakin@gmail.com>
2024-06-28 11:50:05 +0400

    fix: remove redundant masks from adapter tests

--- contracts/test/unit/adapters/curve/CurveV1AdapterStableNG.unit.t.sol
+++ contracts/test/unit/adapters/curve/CurveV1AdapterStableNG.unit.t.sol
@@ -19,18 +19,13 @@ contract CurveV1AdapterStablNGUnitTest is AdapterUnitTestHelper {
     address token2;
     address lpToken;
 
-    uint256 token0Mask;
-    uint256 token1Mask;
-    uint256 token2Mask;
-    uint256 lpTokenMask;
-
     function setUp() public {
         _setUp();
 
-        (token0, token0Mask) = (tokens[0], 1);
-        (token1, token1Mask) = (tokens[1], 2);
-        (token2, token2Mask) = (tokens[2], 4);
-        (lpToken, lpTokenMask) = (tokens[3], 8);
+        token0 = tokens[0];
+        token1 = tokens[1];
+        token2 = tokens[2];
+        lpToken = tokens[3];
 
         address[] memory coins = new address[](3);
         coins[0] = token0;
