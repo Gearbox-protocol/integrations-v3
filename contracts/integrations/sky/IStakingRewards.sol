e3e558df565f3541ca90a2afd40d9e76eded2fa9
Van0k <ivkamakin@gmail.com>
2024-10-17 12:48:45 +0400

    fix: add `earned` to `IStakingRewards`

--- contracts/integrations/sky/IStakingRewards.sol
+++ contracts/integrations/sky/IStakingRewards.sol
@@ -10,4 +10,5 @@ interface IStakingRewards {
     function stake(uint256 amount) external;
     function withdraw(uint256 amount) external;
     function getReward() external;
+    function earned(address account) external view returns (uint256);
 }
