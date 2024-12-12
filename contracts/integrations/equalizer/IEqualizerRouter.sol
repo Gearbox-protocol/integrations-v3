c314c17836d860a3f679e7746885cb031af22d04
Van0k <ivkamakin@gmail.com>
2024-12-12 09:54:14 +0400

    feat: equalizer router adapter (#146)

---
+++ contracts/integrations/equalizer/IEqualizerRouter.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+struct Route {
+    address from;
+    address to;
+    bool stable;
+}
+
+interface IEqualizerRouter {
+    /// @notice Address of the factory
+    function factory() external view returns (address);
+
+    /// @notice Perform chained getAmountOut calculations on any number of pools
+    function getAmountsOut(uint256 amountIn, Route[] memory routes) external view returns (uint256[] memory amounts);
+
+    /// @notice Swap one token for another
+    /// @param amountIn     Amount of token in
+    /// @param amountOutMin Minimum amount of desired token received
+    /// @param routes       Array of trade routes used in the swap
+    /// @param to           Recipient of the tokens received
+    /// @param deadline     Deadline to receive tokens
+    /// @return amounts     Array of amounts returned per route
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        Route[] calldata routes,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
