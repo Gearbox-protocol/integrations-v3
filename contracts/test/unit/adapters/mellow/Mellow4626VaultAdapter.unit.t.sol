af3dfefb78957903b4bca4fdd3771be0486f6485
Van0k <ivkamakin@gmail.com>
2025-07-15 10:18:31 +0400

    feat: mellow 4626 and mellow claimer tests

--- contracts/test/unit/adapters/mellow/Mellow4626VaultAdapter.unit.t.sol
+++ contracts/test/unit/adapters/mellow/Mellow4626VaultAdapter.unit.t.sol
@@ -4,15 +4,15 @@
 pragma solidity ^0.8.23;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Mellow4626VaultAdapter} from "../../../../adapters/mellow/Mellow4626VaultAdapter.sol";
-import {IMellowSimpleLRTVault} from "../../../../integrations/mellow/IMellowSimpleLRTVault.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
-import {IPhantomTokenAdapter} from "../../../../interfaces/IPhantomTokenAdapter.sol";
-import {AdapterUnitTestHelper} from "../AdapterUnitTestHelper.sol";
+import {Mellow4626VaultAdapter} from "../../../../adapters/mellow/Mellow4626VaultAdapter.sol";
+import {IMellowMultiVault} from "../../../../integrations/mellow/IMellowMultiVault.sol";
+import {IMellow4626VaultAdapter} from "../../../../interfaces/mellow/IMellow4626VaultAdapter.sol";
 import {MellowWithdrawalPhantomToken} from "../../../../helpers/mellow/MellowWithdrawalPhantomToken.sol";
+import {AdapterUnitTestHelper} from "../AdapterUnitTestHelper.sol";
 
 /// @title Mellow4626Vault adapter unit test
-/// @notice U:[MV]: Unit tests for Mellow4626Vault adapter
+/// @notice U:[M4626]: Unit tests for Mellow4626Vault adapter
 contract Mellow4626VaultAdapterUnitTest is AdapterUnitTestHelper {
     Mellow4626VaultAdapter adapter;
 
@@ -33,8 +33,8 @@ contract Mellow4626VaultAdapterUnitTest is AdapterUnitTestHelper {
         adapter = new Mellow4626VaultAdapter(address(creditManager), vault, stakedPhantomToken);
     }
 
-    /// @notice U:[MV-1]: Constructor works as expected
-    function test_U_MV_01_constructor_works_as_expected() public {
+    /// @notice U:[M4626-1]: Constructor works as expected
+    function test_U_M4626_01_constructor_works_as_expected() public {
         _readsTokenMask(asset);
         _readsTokenMask(vault);
         _readsTokenMask(stakedPhantomToken);
@@ -43,13 +43,107 @@ contract Mellow4626VaultAdapterUnitTest is AdapterUnitTestHelper {
 
         assertEq(adapter.creditManager(), address(creditManager), "Incorrect creditManager");
         assertEq(adapter.targetContract(), vault, "Incorrect targetContract");
+        assertEq(adapter.vault(), vault, "Incorrect vault");
         assertEq(adapter.asset(), asset, "Incorrect asset");
     }
 
-    function test_U_MV_05_inherited_withdraw_works_as_expected() public {
-        _revertsOnNonFacadeCaller();
-        adapter.withdraw(1000, address(0), address(0));
+    /// @notice U:[M4626-2]: Constructor reverts on invalid multivault
+    function test_U_M4626_02_constructor_reverts_on_invalid_multivault() public {
+        address wrongVault = makeAddr("WRONG_VAULT");
+        vm.mockCall(stakedPhantomToken, abi.encodeWithSignature("multivault()"), abi.encode(wrongVault));
 
+        vm.expectRevert(IMellow4626VaultAdapter.InvalidMultivaultException.selector);
+        new Mellow4626VaultAdapter(address(creditManager), vault, stakedPhantomToken);
+    }
+
+    /// @notice U:[M4626-3]: Wrapper functions revert on wrong caller
+    function test_U_M4626_03_wrapper_functions_revert_on_wrong_caller() public {
+        _revertsOnNonFacadeCaller();
+        adapter.deposit(0, address(0));
+
+        _revertsOnNonFacadeCaller();
+        adapter.depositDiff(0);
+
+        _revertsOnNonFacadeCaller();
+        adapter.mint(0, address(0));
+
+        _revertsOnNonFacadeCaller();
+        adapter.withdraw(0, address(0), address(0));
+
+        _revertsOnNonFacadeCaller();
+        adapter.redeem(0, address(0), address(0));
+
+        _revertsOnNonFacadeCaller();
+        adapter.redeemDiff(0);
+    }
+
+    /// @notice U:[M4626-4]: `deposit` works as expected
+    function test_U_M4626_04_deposit_works_as_expected() public {
+        // Test normal deposit
+        vm.mockCall(vault, abi.encodeCall(IMellowMultiVault.depositWhitelist, ()), abi.encode(false));
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: asset,
+            callData: abi.encodeCall(IERC4626.deposit, (1000, creditAccount)),
+            requiresApproval: true
+        });
+        vm.prank(creditFacade);
+        bool useSafePrices = adapter.deposit(1000, address(0));
+        assertFalse(useSafePrices);
+
+        // Test deposit with whitelist enabled
+        vm.mockCall(vault, abi.encodeCall(IMellowMultiVault.depositWhitelist, ()), abi.encode(true));
+        vm.expectRevert(IMellow4626VaultAdapter.DepositsWhitelistedException.selector);
+        vm.prank(creditFacade);
+        adapter.deposit(1000, address(0));
+    }
+
+    /// @notice U:[M4626-5]: `depositDiff` works as expected
+    function test_U_M4626_05_depositDiff_works_as_expected() public diffTestCases {
+        deal({token: asset, to: creditAccount, give: diffMintedAmount});
+
+        // Test normal depositDiff
+        vm.mockCall(vault, abi.encodeCall(IMellowMultiVault.depositWhitelist, ()), abi.encode(false));
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: asset,
+            callData: abi.encodeCall(IERC4626.deposit, (diffInputAmount, creditAccount)),
+            requiresApproval: true
+        });
+        vm.prank(creditFacade);
+        bool useSafePrices = adapter.depositDiff(diffLeftoverAmount);
+        assertFalse(useSafePrices);
+
+        // Test depositDiff with whitelist enabled
+        vm.mockCall(vault, abi.encodeCall(IMellowMultiVault.depositWhitelist, ()), abi.encode(true));
+        vm.expectRevert(IMellow4626VaultAdapter.DepositsWhitelistedException.selector);
+        vm.prank(creditFacade);
+        adapter.depositDiff(diffLeftoverAmount);
+    }
+
+    /// @notice U:[M4626-6]: `mint` works as expected
+    function test_U_M4626_06_mint_works_as_expected() public {
+        // Test normal mint
+        vm.mockCall(vault, abi.encodeCall(IMellowMultiVault.depositWhitelist, ()), abi.encode(false));
+        _readsActiveAccount();
+        _executesSwap({
+            tokenIn: asset,
+            callData: abi.encodeCall(IERC4626.mint, (1000, creditAccount)),
+            requiresApproval: true
+        });
+        vm.prank(creditFacade);
+        bool useSafePrices = adapter.mint(1000, address(0));
+        assertFalse(useSafePrices);
+
+        // Test mint with whitelist enabled
+        vm.mockCall(vault, abi.encodeCall(IMellowMultiVault.depositWhitelist, ()), abi.encode(true));
+        vm.expectRevert(IMellow4626VaultAdapter.DepositsWhitelistedException.selector);
+        vm.prank(creditFacade);
+        adapter.mint(1000, address(0));
+    }
+
+    /// @notice U:[M4626-7]: `withdraw` works as expected and returns true for safe prices
+    function test_U_M4626_07_withdraw_works_as_expected() public {
         _readsActiveAccount();
         _executesSwap({
             tokenIn: vault,
@@ -58,13 +152,11 @@ contract Mellow4626VaultAdapterUnitTest is AdapterUnitTestHelper {
         });
         vm.prank(creditFacade);
         bool useSafePrices = adapter.withdraw(1000, address(0), address(0));
-        assertTrue(useSafePrices);
+        assertTrue(useSafePrices, "Should use safe prices for withdrawals");
     }
 
-    function test_U_MV_06_inherited_redeem_works_as_expected() public {
-        _revertsOnNonFacadeCaller();
-        adapter.redeem(1000, address(0), address(0));
-
+    /// @notice U:[M4626-8]: `redeem` works as expected and returns true for safe prices
+    function test_U_M4626_08_redeem_works_as_expected() public {
         _readsActiveAccount();
         _executesSwap({
             tokenIn: vault,
@@ -73,13 +165,11 @@ contract Mellow4626VaultAdapterUnitTest is AdapterUnitTestHelper {
         });
         vm.prank(creditFacade);
         bool useSafePrices = adapter.redeem(1000, address(0), address(0));
-        assertTrue(useSafePrices);
+        assertTrue(useSafePrices, "Should use safe prices for redemptions");
     }
 
-    function test_U_MV_07_inherited_redeemDiff_works_as_expected() public diffTestCases {
-        _revertsOnNonFacadeCaller();
-        adapter.redeemDiff(1000);
-
+    /// @notice U:[M4626-9]: `redeemDiff` works as expected and returns true for safe prices
+    function test_U_M4626_09_redeemDiff_works_as_expected() public diffTestCases {
         deal({token: vault, to: creditAccount, give: diffMintedAmount});
 
         _readsActiveAccount();
@@ -90,6 +180,16 @@ contract Mellow4626VaultAdapterUnitTest is AdapterUnitTestHelper {
         });
         vm.prank(creditFacade);
         bool useSafePrices = adapter.redeemDiff(diffLeftoverAmount);
-        assertTrue(useSafePrices);
+        assertTrue(useSafePrices, "Should use safe prices for redemptions");
+    }
+
+    /// @notice U:[M4626-10]: `serialize` works as expected
+    function test_U_M4626_10_serialize_works_as_expected() public {
+        bytes memory serialized = adapter.serialize();
+        (address cm, address tc, address a) = abi.decode(serialized, (address, address, address));
+
+        assertEq(cm, address(creditManager), "Incorrect credit manager in serialized data");
+        assertEq(tc, vault, "Incorrect target contract in serialized data");
+        assertEq(a, asset, "Incorrect asset in serialized data");
     }
 }
