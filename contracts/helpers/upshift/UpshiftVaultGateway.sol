883aad9cffe8ea77258f806b6ebbf34a013d9348
Dima Lekhovitsky <lekhovitsky@gmail.com>
2025-07-25 21:03:49 +0300

    fix: minor improvements in `UpshiftVaultGateway`
    - remove unnecessary token transfers
    - add reentrancy protection

--- contracts/helpers/upshift/UpshiftVaultGateway.sol
+++ contracts/helpers/upshift/UpshiftVaultGateway.sol
@@ -3,8 +3,7 @@
 // (c) Gearbox Foundation, 2025.
 pragma solidity ^0.8.23;
 
-import {IVersion} from "@gearbox-protocol/core-v3/contracts/interfaces/base/IVersion.sol";
-import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {ReentrancyGuardTrait} from "@gearbox-protocol/core-v3/contracts/traits/ReentrancyGuardTrait.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
@@ -19,9 +18,8 @@ struct PendingRedeem {
     uint256 day;
 }
 
-contract UpshiftVaultGateway is IUpshiftVaultGateway {
+contract UpshiftVaultGateway is ReentrancyGuardTrait, IUpshiftVaultGateway {
     using SafeERC20 for IERC20;
-    using EnumerableSet for EnumerableSet.Bytes32Set;
 
     bytes32 public constant override contractType = "GATEWAY::UPSHIFT_VAULT";
     uint256 public constant override version = 3_10;
@@ -37,13 +35,13 @@ contract UpshiftVaultGateway is IUpshiftVaultGateway {
         asset = IERC4626(_upshiftVault).asset();
     }
 
-    function deposit(uint256 assets, address receiver) external {
+    function deposit(uint256 assets, address receiver) external nonReentrant {
         IERC20(asset).safeTransferFrom(msg.sender, address(this), assets);
         IERC20(asset).forceApprove(upshiftVault, assets);
         IERC4626(upshiftVault).deposit(assets, receiver);
     }
 
-    function mint(uint256 shares, address receiver) external {
+    function mint(uint256 shares, address receiver) external nonReentrant {
         uint256 amount = IERC4626(upshiftVault).previewMint(shares);
 
         IERC20(asset).safeTransferFrom(msg.sender, address(this), amount);
@@ -51,13 +49,10 @@ contract UpshiftVaultGateway is IUpshiftVaultGateway {
         IERC4626(upshiftVault).mint(shares, receiver);
     }
 
-    function requestRedeem(uint256 shares) external {
+    function requestRedeem(uint256 shares) external nonReentrant {
         if (pendingRedeems[msg.sender].assets > 0) {
             revert("UpshiftVaultGateway: user has a pending redeem");
         }
-
-        IERC20(upshiftVault).safeTransferFrom(msg.sender, address(this), shares);
-        IERC20(upshiftVault).forceApprove(upshiftVault, shares);
 
         (uint256 year, uint256 month, uint256 day, uint256 claimableTimestamp) =
             IUpshiftVault(upshiftVault).getWithdrawalEpoch();
@@ -67,10 +62,10 @@ contract UpshiftVaultGateway is IUpshiftVaultGateway {
         pendingRedeems[msg.sender] =
             PendingRedeem({claimableTimestamp: claimableTimestamp, assets: assets, year: year, month: month, day: day});
 
-        IUpshiftVault(upshiftVault).requestRedeem(shares, address(this), address(this));
+        IUpshiftVault(upshiftVault).requestRedeem({shares: shares, receiverAddr: address(this), holderAddr: msg.sender});
     }
 
-    function claim(uint256 amount) external {
+    function claim(uint256 amount) external nonReentrant {
         PendingRedeem memory pendingRedeem = pendingRedeems[msg.sender];
 
         if (pendingRedeem.assets == 0) {
