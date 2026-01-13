2da42d899a5b025f5a6d8350496311a693b599b1
Dima Lekhovitsky <lekhovitsky@gmail.com>
2026-01-13 14:18:14 +0200

    fix: remove kodiak

--- script/upload/Upload_2025_08_02_Integrations.s.sol
+++ script/upload/Upload_2025_08_02_Integrations.s.sol
@@ -4,20 +4,16 @@ pragma solidity ^0.8.23;
 import {Bytecode} from "@gearbox-protocol/permissionless/contracts/interfaces/Types.sol";
 import {UploadBytecode} from "@gearbox-protocol/permissionless/script/UploadBytecode.sol";
 
-import {KodiakIslandGatewayAdapter} from "../../contracts/adapters/kodiak/KodiakIslandGatewayAdapter.sol";
 import {Mellow4626VaultAdapter} from "../../contracts/adapters/mellow/Mellow4626VaultAdapter.sol";
 import {MellowClaimerAdapter} from "../../contracts/adapters/mellow/MellowClaimerAdapter.sol";
-import {KodiakIslandGateway} from "../../contracts/helpers/kodiak/KodiakIslandGateway.sol";
 import {MellowWithdrawalPhantomToken} from "../../contracts/helpers/mellow/MellowWithdrawalPhantomToken.sol";
-
-import {KodiakIslandPriceFeed} from "@gearbox-protocol/oracles-v3/contracts/oracles/kodiak/KodiakIslandPriceFeed.sol";
 
 contract Upload_2025_08_02_Integrations is UploadBytecode {
     function _getContracts() internal pure override returns (Bytecode[] memory bytecodes) {
         bytecodes = new Bytecode[](6);
         bytecodes[0].contractType = "ADAPTER::KODIAK_ISLAND_GATEWAY";
         bytecodes[0].version = 3_10;
-        bytecodes[0].initCode = type(KodiakIslandGatewayAdapter).creationCode;
+        // bytecodes[0].initCode = type(KodiakIslandGatewayAdapter).creationCode;
         bytecodes[0].source =
             "https://github.com/Gearbox-protocol/integrations-v3/blob/0d992f9d01f4f936b13ba242e7b6ffaf2a88a976/contracts/adapters/kodiak/KodiakIslandGatewayAdapter.sol";
 
@@ -35,7 +31,7 @@ contract Upload_2025_08_02_Integrations is UploadBytecode {
 
         bytecodes[3].contractType = "GATEWAY::KODIAK_ISLAND";
         bytecodes[3].version = 3_10;
-        bytecodes[3].initCode = type(KodiakIslandGateway).creationCode;
+        // bytecodes[3].initCode = type(KodiakIslandGateway).creationCode;
         bytecodes[3].source =
             "https://github.com/Gearbox-protocol/integrations-v3/blob/0d992f9d01f4f936b13ba242e7b6ffaf2a88a976/contracts/helpers/kodiak/KodiakIslandGateway.sol";
 
@@ -47,7 +43,7 @@ contract Upload_2025_08_02_Integrations is UploadBytecode {
 
         bytecodes[5].contractType = "PRICE_FEED::KODIAK_ISLAND";
         bytecodes[5].version = 3_10;
-        bytecodes[5].initCode = type(KodiakIslandPriceFeed).creationCode;
+        // bytecodes[5].initCode = type(KodiakIslandPriceFeed).creationCode;
         bytecodes[5].source =
             "https://github.com/Gearbox-protocol/oracles-v3/blob/4220625c6f46fdb816f3eb52396d8d62c2b96afe/contracts/oracles/kodiak/KodiakIslandPriceFeed.sol";
     }
